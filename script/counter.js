aed59a7d236a34b7e07078f64d50a68763146262
takumikunn15 <takumikunn15@gmail.com>
2020-06-10 18:35:34 +0900

    スペース込み、無し、スペースだけ追加

--- script/counter.js
+++ script/counter.js
@@ -1,9 +1,12 @@
 let input = document.querySelector("input");
 let log = document.getElementById("log");
+let log2 = document.getElementById("log2");
+let log3 = document.getElementById("log3");
 
 input.oninput = handleInput;
 
 function handleInput(e) {
-    log.textContent = `今入力されている文字は
-      ${e.target.value.length} 文字です。`;
+  log.textContent = `合計 ${e.target.value.length} 文字(スペース込み)`;
+  log2.textContent = `合計 ${e.target.value.replace(/\s+/g, "").length} 文字(スペース無し)`;
+  log3.textContent = `スペース ${e.target.value.length - e.target.value.replace(/\s+/g, "").length} 文字`;
 }
