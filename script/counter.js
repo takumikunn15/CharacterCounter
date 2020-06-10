4df433d3856a5df8bc938fdc3630463b934d08d7
takumikunn15 <takumikunn15@gmail.com>
2020-06-10 19:39:36 +0900

    改行計測追加

--- script/counter.js
+++ script/counter.js
@@ -1,12 +1,19 @@
-let input = document.querySelector("input");
-let log = document.getElementById("log");
+let textarea = document.querySelector("textarea");
+let log1 = document.getElementById("log");
 let log2 = document.getElementById("log2");
 let log3 = document.getElementById("log3");
+let log4 = document.getElementById("log4");
 
-input.oninput = handleInput;
+textarea.oninput = handleInput;
 
 function handleInput(e) {
-  log.textContent = `合計 ${e.target.value.length} 文字(スペース込み)`;
-  log2.textContent = `合計 ${e.target.value.replace(/\s+/g, "").length} 文字(スペース無し)`;
-  log3.textContent = `スペース ${e.target.value.length - e.target.value.replace(/\s+/g, "").length} 文字`;
+  let lineBreak = e.target.value.split("\n").length - 1;
+  let charaSpace = e.target.value.length - lineBreak;
+  let chara = e.target.value.replace(/\s+/g, "").length;
+  let space = charaSpace - chara;
+
+  log.textContent = `合計 ${charaSpace} 文字(スペース込み)`;
+  log2.textContent = `合計 ${chara} 文字(スペース無し)`;
+  log3.textContent = `スペース ${space} 文字`;
+  log4.textContent = `改行 ${lineBreak} 回`;
 }
